7453c017a2f1e4b1dbefe2945e0e85c548da2ba2
Aomaf <aomaf08@gmail.com>
2025-12-18 14:34:08 +0300

    add tracking code for posthog

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import Navbar from "@/components/ui/Navbar";
 import { GlobalData } from "@/types";
 import Footer from "@/components/ui/Footer";
 import { ToastProvider } from "@/components/ui/ToastProvider";
+import Script from "next/script";
 
 const ibmPlexSansArabic = IBM_Plex_Sans_Arabic({
   weight: ["400", "500", "600", "700"],
@@ -40,8 +41,6 @@ export async function generateMetadata(): Promise<Metadata> {
       ? globalMeta.favicon.url
       : `${baseUrl}${globalMeta.favicon.url}`;
 
-    console.log("Full favicon URL: == ", faviconUrl);
-
     metadata.icons = {
       icon: faviconUrl,
       shortcut: faviconUrl,
@@ -59,24 +58,23 @@ export default async function RootLayout({
 }>) {
   const globalData = (await getGlobalData()) as GlobalData;
 
-  // Construct full favicon URL
-  const baseUrl = process.env.NEXT_PUBLIC_URL ?? "http://localhost:1337";
-  const faviconUrl = globalData?.favicon?.url
-    ? globalData.favicon.url.startsWith("http")
-      ? globalData.favicon.url
-      : `${baseUrl}${globalData.favicon.url}`
-    : null;
-
   return (
     <html lang="ar" dir="rtl" className={ibmPlexSansArabic.variable}>
       <head>
-        {faviconUrl && (
-          <>
-            <link rel="icon" type="image/x-icon" href={faviconUrl} />
-            <link rel="shortcut icon" href={faviconUrl} />
-            <link rel="apple-touch-icon" href={faviconUrl} />
-          </>
-        )}
+        {/* PostHog Analytics Script */}
+        <Script
+          id="posthog-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(t,e){var o,n,p,r;e.__SV||(window.posthog && window.posthog.__loaded)||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.crossOrigin="anonymous",p.async=!0,p.src=s.api_host.replace(".i.posthog.com","-assets.i.posthog.com")+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="init Br Vr ci Wr Jr zr qr capture Ni calculateEventProperties Xr register register_once register_for_session unregister unregister_for_session es getFeatureFlag getFeatureFlagPayload isFeatureEnabled reloadFeatureFlags updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures on onFeatureFlags onSurveysLoaded onSessionId getSurveys getActiveMatchingSurveys renderSurvey displaySurvey cancelPendingSurvey canRenderSurvey canRenderSurveyAsync identify setPersonProperties group resetGroups setPersonPropertiesForFlags resetPersonPropertiesForFlags setGroupPropertiesForFlags resetGroupPropertiesForFlags reset get_distinct_id getGroups get_session_id get_session_replay_url alias set_config startSessionRecording stopSessionRecording sessionRecordingStarted captureException loadToolbar get_property getSessionProperty Zr Yr createPersonProfile ts Nr rs opt_in_capturing opt_out_capturing has_opted_in_capturing has_opted_out_capturing get_explicit_consent_status is_capturing clear_opt_in_out_capturing Kr debug O Qr getPageViewId captureTraceFeedback captureTraceMetric Dr".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
+              posthog.init('phc_x0vJlXEK7DdRYbtnORoTtTu2LoUNXcXgVES4gwedHXT', {
+                  api_host: 'https://us.i.posthog.com',
+                  person_profiles: 'identified_only'
+              });
+            `,
+          }}
+        />
       </head>
       <body className="font-sans antialiased relative">
         <ToastProvider>
